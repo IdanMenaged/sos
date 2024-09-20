0ae5c131d78f1f652269c413f2dd96fffb5deb6c
Idan Menaged <idanmenaged@gmail.com>
2024-09-20 14:41:12 +0300

    added function to contact server on a separate thread

--- sender/app/src/main/java/com/example/sender/MainActivity.kt
+++ sender/app/src/main/java/com/example/sender/MainActivity.kt
@@ -2,26 +2,27 @@ package com.example.sender
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.RelativeLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.sender.ui.theme.SenderTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.io.OutputStream
+import java.net.InetSocketAddress
 import java.net.Socket
 
-private const val SERVER_IP = "82.166.89.189" // needs to be changed depending on the server ip
+private const val SERVER_IP = "placeholder" // needs to be changed depending on the server ip
 private const val SERVER_PORT = 1234 // needs to match the port server is running on
+private const val TIMEOUT = 10000
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,24 +44,26 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun SosButton() {
     Button(onClick = {
-        Log.d("sosbtn", "pressed")
+        CoroutineScope(Dispatchers.IO).launch {
+            sendMessageToServer("echo hi")
+        }
     }) {
         Text("SOS")
     }
 }
 
-@Preview(showBackground = false)
-@Composable
-fun SosButtonPreview() {
-    SenderTheme {
-        SosButton()
+private fun sendMessageToServer(msg: String) {
+    try {
+        val socket = Socket()
+        socket.connect(InetSocketAddress(SERVER_IP, SERVER_PORT), TIMEOUT)
+
+        val outputStream: OutputStream = socket.getOutputStream()
+        outputStream.write(msg.toByteArray())
+        outputStream.flush()
+
+        outputStream.close()
+        socket.close()
+    } catch (e: Exception) {
+        Log.e("sosbtn", "error in sendMessageToServer", e)
     }
 }
-
-//fun sendMessageToServer(ip: String, port: Int, message: String) {
-//    var socket: Socket? = null
-//    var outputStream: OutputStream? = null
-//
-//    outputStream.write(message.toByteArray())
-//    outputStream.flush()
-//}
