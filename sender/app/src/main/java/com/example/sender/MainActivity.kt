307ec6c43331b32e9a13e2c3cb61443dba58a46d
Idan Menaged <idanmenaged@gmail.com>
2024-10-03 13:13:38 +0300

    separate into classes

--- sender/app/src/main/java/com/example/sender/MainActivity.kt
+++ sender/app/src/main/java/com/example/sender/MainActivity.kt
@@ -21,16 +21,18 @@ import java.io.OutputStream
 import java.net.InetSocketAddress
 import java.net.Socket
 
-private const val SERVER_IP = "10.0.2.2" // special built in port that directs to development
-// machine (i.e. computer hosting the emulator)
+private const val SERVER_IP = "10.0.2.2" // special built-in port that directs to development machine
 private const val SERVER_PORT = 4000 // needs to match the port server is running on
 private const val MSG_LEN_PADDING = 4 // for formatting messages in a way the server can understand
 private const val TIMEOUT = 10000
 
 /**
- * UI of the app
+ * Main activity for the app
  */
 class MainActivity : ComponentActivity() {
+    /**
+     * defines the ui
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -45,109 +47,111 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
 
-/**
- * defines the SOS button
- */
-@Composable
-fun SosButton() {
-    Button(onClick = {
-        CoroutineScope(Dispatchers.IO).launch {
-            sendNRecv("echo hi")
+    /**
+     * defines the SOS button element
+     */
+    @Composable
+    fun SosButton() {
+        Button(onClick = {
+            CoroutineScope(Dispatchers.IO).launch {
+                val serverCommunicator = ServerCommunicator()
+                serverCommunicator.sendNRecv("echo hi")
+            }
+        }) {
+            Text("SOS")
         }
-    }) {
-        Text("SOS")
     }
 }
 
 /**
- * sends a message to the server and receives a response
- * msg: string - the message to send (i.e. 'echo hi')
+ * Handles server communication
  */
-private fun sendNRecv(msg: String) {
-    var socket: Socket? = null
-    var outputStream: OutputStream? = null
-    var inputStream: InputStream? = null
-    try {
-        // Create socket and set timeout
-        socket = Socket()
-        socket.soTimeout = TIMEOUT
-        socket.connect(InetSocketAddress(SERVER_IP, SERVER_PORT), TIMEOUT)
-
-        // Send message to server
-        outputStream = socket.getOutputStream()
-        sendMessageToServer(msg, outputStream)
-
-        // Receive message from server
-        inputStream = socket.getInputStream()
-        val response = receiveMessageFromServer(inputStream)
-
-        Log.d("sosbtn", "response received: $response")
-
-    } catch (e: Exception) {
-        Log.e("sosbtn", "error in send_n_recv", e)
-    } finally {
-        // Close streams and socket after communication is done
+class ServerCommunicator {
+    /**
+     * send a request to the server and receive a response.
+     * logs the response.
+     * msg (String): message to send to the server
+     */
+    fun sendNRecv(msg: String) {
+        var socket: Socket? = null
+        var outputStream: OutputStream? = null
+        var inputStream: InputStream? = null
         try {
-            outputStream?.close()
-            inputStream?.close()
-            socket?.close()
+            // Create socket and set timeout
+            socket = Socket()
+            socket.soTimeout = TIMEOUT
+            socket.connect(InetSocketAddress(SERVER_IP, SERVER_PORT), TIMEOUT)
+
+            // Send message to server
+            outputStream = socket.getOutputStream()
+            sendMessageToServer(msg, outputStream)
+
+            // Receive message from server
+            inputStream = socket.getInputStream()
+            val response = receiveMessageFromServer(inputStream)
+
+            Log.d("ServerCommunicator", "Response received: $response")
+
         } catch (e: Exception) {
-            Log.e("sosbtn", "error closing resources", e)
+            Log.e("ServerCommunicator", "Error in sendNRecv", e)
+        } finally {
+            // Close streams and socket after communication is done
+            try {
+                outputStream?.close()
+                inputStream?.close()
+                socket?.close()
+            } catch (e: Exception) {
+                Log.e("ServerCommunicator", "Error closing resources", e)
+            }
         }
     }
-}
 
-/**
- * sends a message to the server
- * msg: string - message to send (i.e. 'echo hi')
- * outputStream: OutputStream - the output stream of the socket
- */
-private fun sendMessageToServer(msg: String, outputStream: OutputStream) {
-    val formattedMsg = formatMessage(msg)
-    try {
-        // Send the message
-        outputStream.write(formattedMsg)
-        outputStream.flush()
-    } catch (e: Exception) {
-        Log.e("sosbtn", "error in sendMessageToServer", e)
-    }
-}
-
-
-/**
- * receives a message from the server (i.e. if request was 'echo hi' the message would be 'hi')
- * inputStream: InputStream - the socket's input stream
- * returns: String - the received message
- */
-private fun receiveMessageFromServer(inputStream: InputStream): String? {
-    try {
-        // Get the message length
-        val msg_len_bytes = ByteArray(MSG_LEN_PADDING)
-        inputStream.read(msg_len_bytes)
-        val msg_len = String(msg_len_bytes).trim().toInt()
-
-        // Read the actual message
-        val message_bytes = ByteArray(msg_len)
-        inputStream.read(message_bytes)
-
-        return String(message_bytes)
-    } catch (e: Exception) {
-        Log.e("sosbtn", "error in receiveMessageFromServer", e)
+    /**
+     * send a message to the server
+     * msg (String): message to send to the server
+     * outputStream (OutputStream): output stream of the socket
+     */
+    private fun sendMessageToServer(msg: String, outputStream: OutputStream) {
+        val formattedMsg = formatMessage(msg)
+        try {
+            // Send the message
+            outputStream.write(formattedMsg)
+            outputStream.flush()
+        } catch (e: Exception) {
+            Log.e("ServerCommunicator", "Error in sendMessageToServer", e)
+        }
     }
 
-    return null
-}
+    /**
+     * receive a message from the server.
+     * inputStream (InputStream): input stream of the socket
+     * returns (String): message received
+     */
+    private fun receiveMessageFromServer(inputStream: InputStream): String? {
+        try {
+            // Get the message length
+            val msgLenBytes = ByteArray(MSG_LEN_PADDING)
+            inputStream.read(msgLenBytes)
+            val msgLen = String(msgLenBytes).trim().toInt()
 
+            // Read the actual message
+            val messageBytes = ByteArray(msgLen)
+            inputStream.read(messageBytes)
 
+            return String(messageBytes)
+        } catch (e: Exception) {
+            Log.e("ServerCommunicator", "Error in receiveMessageFromServer", e)
+        }
 
-/**
- * adds the length in front of the msg and converts it to bytes
- * i.e. hi -> 0002hi
- */
-private fun formatMessage(msg: String): ByteArray {
-    val lengthString = msg.length.toString().padStart(MSG_LEN_PADDING, '0')
-    val result = lengthString.toByteArray(Charsets.UTF_8) + msg.toByteArray(Charsets.UTF_8)
-    return result
+        return null
+    }
+
+    /**
+     * adds a prefix to the message to denote it's length
+     */
+    private fun formatMessage(msg: String): ByteArray {
+        val lengthString = msg.length.toString().padStart(MSG_LEN_PADDING, '0')
+        return lengthString.toByteArray(Charsets.UTF_8) + msg.toByteArray(Charsets.UTF_8)
+    }
 }
