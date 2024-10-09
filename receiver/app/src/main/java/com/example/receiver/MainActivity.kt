681cb7e23d05bf4c1f667da205253d6237d0e917
Idan Menaged <idanmenaged@gmail.com>
2024-10-09 13:32:44 +0300

    service runs after app termination

--- receiver/app/src/main/java/com/example/receiver/MainActivity.kt
+++ receiver/app/src/main/java/com/example/receiver/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.receiver
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
 import android.os.Bundle
@@ -51,7 +54,7 @@ class ServerCommunicator : Service() {
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Thread {
             while (true) {
-                Log.d("ServerCommunicator", "Service is running...")
+                Log.d("Service", "Service is running...")
                 try {
                     Thread.sleep(2000)
                 } catch (e: InterruptedException) {
@@ -60,8 +63,24 @@ class ServerCommunicator : Service() {
             }
         }.start()
 
+        val CHANNEL_ID = "Foreground Service ID"
+        val channel = NotificationChannel(
+            CHANNEL_ID,
+            CHANNEL_ID,
+            NotificationManager.IMPORTANCE_LOW
+        )
+
+        getSystemService(NotificationManager::class.java).createNotificationChannel(channel)
+        val notification = Notification.Builder(this, CHANNEL_ID)
+            .setContentText("Service is running")
+            .setContentTitle("Service enabled")
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .build()
+
+        startForeground(1001, notification)
         return super.onStartCommand(intent, flags, startId)
     }
+
 
 
     override fun onBind(intent: Intent?): IBinder? {
