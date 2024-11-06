9ee52c4f04a402b4530cbc538abda558bca5c354
Idan Menaged <idanmenaged@gmail.com>
2024-11-06 10:39:22 +0200

    added ayelet's todos

--- sender/app/src/main/java/com/example/sender/Listener.kt
+++ sender/app/src/main/java/com/example/sender/Listener.kt
@@ -8,7 +8,7 @@ package com.example.sender
  * a class that listens to messages from the server on a different socket and thread than the one
  * sending messages. responsible for receiving the sos messages of others
  */
-class Listener {
+class Listener {  // todo: make inheritance
     private val serverCommunicator = ServerCommunicator()
 
     init {
