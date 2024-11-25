a86df0e47cdf442917b2ac5ecdc6b2015a96d095
Idan Menaged <idanmenaged@gmail.com>
2024-11-25 11:39:24 +0200

    part way through handling async

--- sender/app/src/main/java/com/example/sender/data/LoginDataSource.kt
+++ sender/app/src/main/java/com/example/sender/data/LoginDataSource.kt
@@ -5,6 +5,7 @@ import com.example.sender.data.model.LoggedInUser
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.io.IOException
 
 /**
@@ -13,27 +14,24 @@ import java.io.IOException
 class LoginDataSource {
     private val serverCommunicator = ServerCommunicator()
 
-    fun login(username: String, password: String, callback: (Result<LoggedInUser>) -> Unit) {
-        // todo: modify the rest of the code to use the callback
+    suspend fun login(username: String, password: String): Result<LoggedInUser> {
         try {
-            // Launch a new coroutine for background operation
-            CoroutineScope(Dispatchers.IO).launch {
-                // Perform the login operation asynchronously
-                val res = serverCommunicator.sendNRecv("login $username $password")
+            val res = withContext(Dispatchers.IO) {
+                serverCommunicator.sendNRecv("login $username $password")
+            }
 
-                // Handle result based on server response
-                if (res == "success") {
-                    val user = LoggedInUser(java.util.UUID.randomUUID().toString(), username)
-                    // Invoke the callback with success
-                    callback(Result.Success(user))
-                } else {
-                    // Invoke the callback with an error result
-                    callback(Result.Error(Exception("Wrong credentials")))
-                }
+            // Handle result based on server response
+            if (res == "success") {
+                val user = LoggedInUser(java.util.UUID.randomUUID().toString(), username)
+                // Invoke the callback with success
+                return Result.Success(user)
+            } else {
+                // Invoke the callback with an error result
+                return Result.Error(Exception("Wrong credentials"))
             }
         } catch (e: Throwable) {
             // In case of failure, invoke callback with error
-            callback(Result.Error(IOException("Error logging in", e)))
+            return Result.Error(IOException("Error logging in", e))
         }
     }
 
