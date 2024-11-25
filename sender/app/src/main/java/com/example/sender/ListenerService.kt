9c31129306888618aa1302fe553425cec7aa9f44
Idan Menaged <idanmenaged@gmail.com>
2024-11-25 11:05:47 +0200

    ListenerService works in bg

--- sender/app/src/main/java/com/example/sender/ListenerService.kt
+++ sender/app/src/main/java/com/example/sender/ListenerService.kt
@@ -1,10 +1,15 @@
 package com.example.sender
 
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.os.IBinder
 import android.util.Log
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -14,6 +19,7 @@ class ListenerService: Service() {
         return null
     }
 
+    @SuppressLint("NewApi")
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Toast.makeText(
             applicationContext, "Listener service has started running in the background",
@@ -21,6 +27,22 @@ class ListenerService: Service() {
         ).show()
 
         Log.d("ListenerService","Starting Service")
+
+        val notificationChannel = NotificationChannel("listener_service_channel",
+            "Listener Service", NotificationManager.IMPORTANCE_DEFAULT).apply {
+                description = "service that listens to the server"
+        }
+
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(notificationChannel)
+
+        val notification = NotificationCompat.Builder(this, "listener_service_channel")
+            .setContentTitle("Listener Service")
+            .setContentText("Listening for events")
+            .build()
+
+        startForeground(1, notification)
 
         CoroutineScope(Dispatchers.IO).launch {
             val listener = Listener()
