40a24b789de26108bed6c9af48360b2389138fd3
IdanMenaged <idanmenaged@gmail.com>
2024-12-23 17:04:40 +0200

    add_connection function

--- server/social.py
+++ server/social.py
@@ -3,27 +3,28 @@ from auth import DBManager
 
 def main():
     s = Social()
-    s.add_connection(1, 2)
+    print(s.add_connection(1, 2))
 
 
 class Social(DBManager):
-    # TODO: finish add_connection, write remove_connection function
+    # TODO: write remove_connection function
     def add_connection(self, befriender: int, befriended: int):
         """
         add the befriended id to the befriender's connections field
         :param befriender: id of the befriender
         :param befriended: id of the befriended
+        :return true on success, false on failure
         """
-        # TODO: get existing data, add befriended, update db
         q = f'SELECT connections FROM users WHERE id = ?'
         res = self.exec(q, befriender)
         connections = res[0][0]
         if connections is None:
-            connections = ''
+            connections = befriended
+        else:
+            connections += f',{befriended}'  # TODO: handle duplicate ids
 
-        connections += f', {befriended}'
-
-        q = ''
+        q = 'UPDATE users SET connections = ? WHERE id = ?'
+        self.exec(q, connections, befriender)
 
 
 if __name__ == '__main__':
