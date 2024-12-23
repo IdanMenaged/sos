6fd62693ba5bb6db94e81fe5d9995c2c29aa8bd2
IdanMenaged <idanmenaged@gmail.com>
2024-12-23 16:40:27 +0200

    started social handling

--- server/social.py
+++ server/social.py
@@ -1,6 +1,11 @@
 from auth import DBManager
 
 
+def main():
+    s = Social()
+    s.add_connection(1, 2)
+
+
 class Social(DBManager):
     # TODO: finish add_connection, write remove_connection function
     def add_connection(self, befriender: int, befriended: int):
@@ -10,3 +15,16 @@ class Social(DBManager):
         :param befriended: id of the befriended
         """
         # TODO: get existing data, add befriended, update db
+        q = f'SELECT connections FROM users WHERE id = ?'
+        res = self.exec(q, befriender)
+        connections = res[0][0]
+        if connections is None:
+            connections = ''
+
+        connections += f', {befriended}'
+
+        q = ''
+
+
+if __name__ == '__main__':
+    main()
