103dc1156f6682c2122376e8b1f2b958c64b999c
IdanMenaged <idanmenaged@gmail.com>
2025-01-05 16:00:27 +0200

    removed unneeded methods from methods.py

--- server/dummy_client/methods.py
+++ server/dummy_client/methods.py
@@ -3,8 +3,6 @@ Idan Menaged
 methods the server can execute. not an exhaustive list.
 """
 
-import threading
-
 SCREENSHOT_PATH = 'c:\\test_folder\\server\\screen.jpg'
 FILE_PATH = 'c:\\test_folder\\client'
 
@@ -13,37 +11,6 @@ class Methods:
     """
     methods server can execute
     """
-    hist = {}  # history of commands sent in the format of { (ip, port): ['placeholder'] }
-    lock = threading.Lock()
-
-    @staticmethod
-    def new_hist(addr):
-        """
-        insert new entry to history
-        """
-        Methods.lock.acquire()
-        Methods.hist[addr] = []
-        Methods.lock.release()
-
-    @staticmethod
-    def add_to_hist(addr, req):
-        """
-        add request to history
-        """
-        Methods.lock.acquire()
-        Methods.hist[addr].append(req)
-        Methods.lock.release()
-
-    @staticmethod
-    def history(addr):
-        """
-        list the command history
-        """
-        Methods.lock.acquire()
-        res = str(Methods.hist[addr])
-        Methods.lock.release()
-        return res
-
     @staticmethod
     def echo(msg):
         """
