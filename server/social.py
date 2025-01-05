fb686bf750dd3598fc851fba3a2da2cd0dbe7759
IdanMenaged <idanmenaged@gmail.com>
2025-01-05 14:26:27 +0200

    removed unneeded code

--- server/social.py
+++ server/social.py
@@ -1,40 +1,12 @@
-from calendar import firstweekday
-
 from auth import DBManager
 
 
 def main():
     s = Social()
-    print(s.add_connection("idan", "ayelet"))
     print(s.get_connections("idan"))
 
 
 class Social(DBManager):
-    def add_connection(self, befriender: str, befriended: str):
-        """
-        add the befriended name to the befriender's connections field
-        :param befriender: name of the befriender
-        :param befriended: name of the befriended
-        :return true on success, false on failure
-        """
-        try:
-            q = f'SELECT connections FROM users WHERE name = ?'
-            res = self.exec(q, befriender)
-            connections = res[0][0]
-            if connections is None:
-                connections = befriended
-            else:
-                connections = set(connections.split(','))
-                connections.add(str(befriended))
-                connections = ','.join(connections)
-
-            q = 'UPDATE users SET connections = ? WHERE name = ?'
-            self.exec(q, connections, befriender)
-        except Exception as e:
-            print(e)
-            return 'False'
-        return 'True'
-
     def get_connections(self, name: str):
         """
         get all connections
