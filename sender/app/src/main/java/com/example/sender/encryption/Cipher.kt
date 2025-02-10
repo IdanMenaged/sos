ab9bd4aa07ee51f1437ab560317aa51d442d33be
IdanMenaged <idanmenaged@gmail.com>
2025-02-10 10:55:09 +0200

    is length prefix right?

--- sender/app/src/main/java/com/example/sender/encryption/Cipher.kt
+++ sender/app/src/main/java/com/example/sender/encryption/Cipher.kt
@@ -1,6 +1,7 @@
 package com.example.sender.encryption
 
 import android.annotation.SuppressLint
+import android.util.Log
 import java.net.Socket
 import java.io.InputStream
 import java.io.OutputStream
@@ -8,6 +9,8 @@ import java.util.Base64
 
 class Cipher {
     companion object {
+        const val TAG = "Cipher"
+
         fun sendRecvKey(conn: Socket): ByteArray {
             val dh = DiffieHellman()
             sendBin(conn.getOutputStream(), dh.publicKey) // DH public key
@@ -24,17 +27,27 @@ class Cipher {
             return dh.generateSharedSecret(dhKeyBytes)
         }
 
+        // todo: magic numbers
         @SuppressLint("NewApi")
         private fun sendBin(outputStream: OutputStream, data: ByteArray) {
-            val encodedData = Base64.getEncoder().encodeToString(data)
-            outputStream.write(encodedData.toByteArray())
+            val lengthPrefix = "%04d".format(data.size) // Ensures a 4-character length prefix
+            val encodedData = lengthPrefix.toByteArray(Charsets.UTF_8) + data
+            Log.d(TAG, String(encodedData))
+            outputStream.write(encodedData)
             outputStream.flush()
         }
 
         @SuppressLint("NewApi")
         private fun recvBin(inputStream: InputStream): ByteArray {
-            val receivedData = inputStream.bufferedReader().readLine()
-            return Base64.getDecoder().decode(receivedData)
+            val lengthBuffer = ByteArray(4)
+            inputStream.read(lengthBuffer, 0, 4) // Read the first 4 bytes for the length
+            val lengthString = lengthBuffer.toString(Charsets.UTF_8)
+            val messageLength = lengthString.trim().toInt() // Convert length string to an integer
+
+            val messageBuffer = ByteArray(messageLength)
+            inputStream.read(messageBuffer, 0, messageLength) // Read the exact number of bytes
+            return messageBuffer
         }
+
     }
 }
